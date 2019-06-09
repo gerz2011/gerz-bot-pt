8f3d94176e1afe306aa57655208ef564742290d8
Gerz2011 <30986978+gerz2011@users.noreply.github.com>
2019-06-09 13:53:05 +0300

    Update profibot.py

--- profibot.py
+++ profibot.py
@@ -1,10 +1,17 @@
+import os
 import telebot
 # from telebot import apihelper
 from telebot import types
-# import setings as set
+from flask import Flask, request
+
+
+
+TOKEN = '781098537:AAEGQ7-kRv6Pt8KGs5CfW9RiPRLU8lKHp58'
+server = Flask(__name__)
+bot = telebot.TeleBot(TOKEN)
 
 # apihelper.proxy = {'https':'https://188.216.77.95:8118'}
-bot = telebot.TeleBot('781098537:AAEGQ7-kRv6Pt8KGs5CfW9RiPRLU8lKHp58')
+
 
 @bot.message_handler(commands=['start'])
 def fun_start(m):
@@ -31,3 +38,19 @@ def fun_answer(m):
         bot.send_message(m.from_user.id, 'price replase glass macbook')
 
 bot.polling()
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://gerz-bot-pt.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
